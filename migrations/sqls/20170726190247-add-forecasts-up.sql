a05eec5d253d135e4d60b02e7a15b1cf23ba9c4c
Tom Hudson <tomist@gmail.com>
2017-07-31 12:40:27 -0500

    Request from DG

--- migrations/sqls/20170726190247-add-forecasts-up.sql
+++ migrations/sqls/20170726190247-add-forecasts-up.sql
@@ -5,7 +5,7 @@ CREATE TABLE web.forecasted_rundowns (
   day DATE NOT NULL,
   confidence REAL NOT NULL,
   closing_stock INT,
-  closing_bottom INT,
-  closing_top INT,
+  closing_stock_low INT,
+  closing_stock_high INT,
   created_at TIMESTAMP DEFAULT NOW()
 );
