b29a06435612de666f21d11f852f69de6aac695e
Anthony Shull <shull.anthony@gmail.com>
2017-08-07 17:39:29 +0000

    Add origin and destination name to movements query

--- plugins/locations/queries/get_terminal_movements.sql
+++ plugins/locations/queries/get_terminal_movements.sql
@@ -1,7 +1,15 @@
-SELECT m.* FROM web.terminals t
-JOIN web.locations l ON l.ref_id = t.id AND l.type = t.type
-JOIN web.movements m ON m.origin_id = l.id OR m.destination_id = l.id
+SELECT m.*, g.name AS grade_name, l3.name AS origin_name, l4.name AS destination_name FROM web.movements m
+JOIN web.locations l1 ON m.origin_id = l1.id
+JOIN web.locations l2 ON m.destination_id = l2.id
 JOIN web.grades g ON m.grade_id = g.id
-WHERE t.id = $1
+JOIN (
+(SELECT id, type, name FROM web.refineries) UNION (SELECT id, type, name FROM web.terminals) UNION (SELECT id, type, name FROM web.stations)
+) AS l3
+ON l1.ref_id = l3.id AND l1.type = l3.type
+JOIN (
+(SELECT id, type, name FROM web.refineries) UNION (SELECT id, type, name FROM web.terminals) UNION (SELECT id, type, name FROM web.stations)
+) AS l4
+ON l2.ref_id = l4.id AND l2.type = l4.type
+WHERE ((l3.id = $1 AND l3.type = 'terminal') OR (l4.id = $1 AND l4.type = 'terminal'))
 AND m.grade_id = $2
 AND m.day BETWEEN $3 AND $4;
