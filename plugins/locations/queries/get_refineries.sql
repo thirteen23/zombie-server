f138f5225fd3ac334e8a45154e48634a2b149f1f
Anthony Shull <shull.anthony@gmail.com>
2017-08-08 21:18:17 +0000

    Operator name

--- plugins/locations/queries/get_refineries.sql
+++ plugins/locations/queries/get_refineries.sql
@@ -1,6 +1,6 @@
 SELECT r.*,
 c1.id AS owner_id, c1.name AS owner_name,
-c2.id AS operator_id, c2.name AS operator_id
+c2.id AS operator_id, c2.name AS operator_name
 FROM web.refineries r
 JOIN web.locations l ON l.ref_id = r.id
 JOIN web.companies c1 ON r.owner_id = c1.id
