dd2e3404f3ad6e5cee80ddc6f3866b4e3b247e35
Anthony Shull <shull.anthony@gmail.com>
2017-08-09 20:59:52 +0000

    Eleven days of forecasts

--- plugins/locations/queries/get_terminal_forecast_rundowns.sql
+++ plugins/locations/queries/get_terminal_forecast_rundowns.sql
@@ -2,6 +2,6 @@ SELECT DISTINCT (f.terminal_id, f.grade_id, f.day), f.*
 FROM web.forecasted_rundowns f
 WHERE f.terminal_id = $1
 AND f.grade_id = $2
-AND f.day > '20170401'
+AND f.day > '20170331'
 ORDER BY f.created_at DESC
-LIMIT 10;
+LIMIT 11;
