b71d7b61bea58405767bc63db12c27c58589b379
Anthony Shull <shull.anthony@gmail.com>
2017-08-29 09:57:52 -0500

    Migrations and seed data

---
+++ seed/nominations.js
@@ -0,0 +1,23 @@
+const config = require('config');
+const csv = require('csv-streamify');
+const fs = require('fs');
+
+const { Pool } = require('pg');
+
+const pool = new Pool(config.get('pg'));
+
+exports.seed = () => {
+  let rows = 0;
+  fs.createReadStream(`${__dirname}/categories.csv`).pipe(csv({objectMode: true, columns: true}))
+    .on('data', (row) => {
+      const name = row.name;
+      rows++;
+      pool.query('INSERT INTO categories(name) VALUES($1)', [name], (err, res) => {
+        if (err) console.error(err);
+        if (res) {
+          rows--;
+        }
+        if (rows === 0) process.exit();
+      });
+    });
+};
--- sql/V12__add_nominations.sql
+++ sql/V12__add_nominations.sql
@@ -1,12 +1,12 @@
 CREATE TABLE web.nominations (
   id SERIAL PRIMARY KEY,
-  segment_id INT REFERENCES segments(id) NOT NULL,
-  grade_id INT REFERENCES grades(id) NOT NULL,
+  segment_id INT REFERENCES web.segments(id) NOT NULL,
+  grade_id INT REFERENCES web.grades(id) NOT NULL,
   start TIMESTAMP WITH TIME ZONE NOT NULL CHECK (start < stop),
   stop TIMESTAMP WITH TIME ZONE NOT NULL CHECK (stop > start),
   contracted_volume INT CHECK (contracted_volume > 0),
   nominated_min INT CHECK (nominated_min > 0),
   nominated_max INT CHECK (nominated_max > 0),
   tariff_min INT CHECK (tariff_min > 0),
-  tariff_max INT CHECK (tariff_max > 0),
+  tariff_max INT CHECK (tariff_max > 0)
 );
