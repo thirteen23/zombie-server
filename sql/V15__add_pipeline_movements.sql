6d5757ef4d18dd9c5fbabd95c3503c4cee6da8e5
Anthony Shull <shull.anthony@gmail.com>
2017-09-26 16:50:33 -0500

    Update V15__add_pipeline_movements.sql

--- sql/V15__add_pipeline_movements.sql
+++ sql/V15__add_pipeline_movements.sql
@@ -5,6 +5,7 @@ CREATE TABLE web.pipeline_movements (
   path_id INT REFERENCES web.paths(id) NOT NULL,
   grade_id INT REFERENCES web.grades(id) NOT NULL,
   volume INT NOT NULL CHECK (volume >= 0),
+  pump_rate INT (pump_rate >= 0),
   origin_start TIMESTAMP WITH TIME ZONE CHECK (origin_start < origin_end),
   origin_end TIMESTAMP WITH TIME ZONE CHECK (origin_end > origin_start),
   destination_start TIMESTAMP WITH TIME ZONE CHECK (destination_start < destination_end),
