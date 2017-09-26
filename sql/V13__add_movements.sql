37ea232e84041b364d66ce0186299554a1eafac1
Anthony Shull <shull.anthony@gmail.com>
2017-09-26 09:52:52 -0500

    Remove not null checks from movements

--- sql/V13__add_movements.sql
+++ sql/V13__add_movements.sql
@@ -7,10 +7,10 @@ CREATE TABLE web.movements (
   origin_id INT REFERENCES web.locations(id) NOT NULL,
   destination_id INT REFERENCES web.locations(id) NOT NULL,
   grade_id INT REFERENCES web.grades(id) NOT NULL,
-  type web.movement_type NOT NULL,
+  type web.movement_type,
   volume INT NOT NULL CHECK (volume >= 0),
-  origin_start TIMESTAMP WITH TIME ZONE NOT NULL CHECK (origin_start < origin_end),
-  origin_end TIMESTAMP WITH TIME ZONE NOT NULL CHECK (origin_end > origin_start),
+  origin_start TIMESTAMP WITH TIME ZONE CHECK (origin_start < origin_end),
+  origin_end TIMESTAMP WITH TIME ZONE CHECK (origin_end > origin_start),
   destination_start TIMESTAMP WITH TIME ZONE CHECK (destination_start < destination_end)
   destination_end TIMESTAMP WITH TIME ZONE CHECK (destination_end > destination_start)
 );
