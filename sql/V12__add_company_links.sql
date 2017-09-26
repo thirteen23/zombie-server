7f867ed7941f5de951562322d1c3d2dfb25454e7
Robert Gonzalez <robert@lightjab.com>
2017-09-26 10:03:42 -0500

    Update V12__add_company_links.sql
    Updating the script to reference the correct web.companies

--- sql/V12__add_company_links.sql
+++ sql/V12__add_company_links.sql
@@ -3,7 +3,7 @@ CREATE TYPE web.company_link_type AS ENUM ('nomination system', 'product specifi
 -- Static
 CREATE TABLE web.company_links (
   id SERIAL PRIMARY KEY,
-  company_id INT REFERENCES companies(id) NOT NULL,
+  company_id INT REFERENCES web.companies(id) NOT NULL,
   type web.company_link_type NOT NULL,
   url VARCHAR NOT NULL
 );
