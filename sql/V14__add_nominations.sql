d12e5a50f5eed94c21c0faeebe2c690de82c73d4
Tom Hudson <tomist@gmail.com>
2017-09-27 14:27:08 -0500

    Update V14__add_nominations.sql

--- sql/V14__add_nominations.sql
+++ sql/V14__add_nominations.sql
@@ -5,9 +5,9 @@ CREATE TABLE web.nominations (
   grade_id INT REFERENCES web.grades(id) NOT NULL,
   start DATE NOT NULL CHECK (start < "end"),
   "end" DATE NOT NULL CHECK ("end" > start),
-  contracted_volume INT CHECK (contracted_volume > 0),
-  nominated_min INT CHECK (nominated_min > 0),
-  nominated_max INT CHECK (nominated_max > 0),
-  tariff_min INT CHECK (tariff_min > 0),
-  tariff_max INT CHECK (tariff_max > 0)
+  contracted_volume INT,
+  nominated_min INT,
+  nominated_max INT,
+  tariff_min INT,
+  tariff_max INT
 );
