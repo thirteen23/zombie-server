f736f3411468272b937ebff503dcf321f8221f9b
Tom Hudson <tomist@gmail.com>
2017-09-27 13:35:00 -0500

    Update V15__add_pipeline_movements.sql

--- sql/V15__add_pipeline_movements.sql
+++ sql/V15__add_pipeline_movements.sql
@@ -1,13 +1,13 @@
 -- Dynamic
 CREATE TABLE web.pipeline_movements (
   id SERIAL PRIMARY KEY,
-  transaction VARCHAR,
   path_id INT REFERENCES web.paths(id) NOT NULL,
   grade_id INT REFERENCES web.grades(id) NOT NULL,
   volume INT NOT NULL CHECK (volume >= 0),
-  pump_rate INT CHECK (pump_rate >= 0),
+  transaction VARCHAR,
   origin_start TIMESTAMP WITH TIME ZONE CHECK (origin_start < origin_end),
   origin_end TIMESTAMP WITH TIME ZONE CHECK (origin_end > origin_start),
   destination_start TIMESTAMP WITH TIME ZONE CHECK (destination_start < destination_end),
-  destination_end TIMESTAMP WITH TIME ZONE CHECK (destination_end > destination_start)
+  destination_end TIMESTAMP WITH TIME ZONE CHECK (destination_end > destination_start),
+  pump_rate INT CHECK (pump_rate >= 0)
 );
