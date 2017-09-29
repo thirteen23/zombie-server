c625aeb5c145add73195730ff17e99a349283f2e
Robert Gonzalez <robert@lightjab.com>
2017-09-29 16:07:57 -0500

    Updating stations

--- seed/stations.js
+++ seed/stations.js
@@ -3,30 +3,27 @@ const csv = require('csv-streamify');
 const fs = require('fs');
 
 const { Pool } = require('pg');
-
 const pool = new Pool(config.get('pg'));
 
 let rows = 0;
-
 exports.seed = () => {
-
-  fs.createReadStream(`${__dirname}/stations.csv`).pipe(csv({objectMode: true, columns: true}))
-    .on('data', (row) => {
-      rows++;
-      (async () => {
-        const station = {
-          name: row.locationName,
-          latitude: parseFloat(row.latitude),
-          longitude: parseFloat(row.longitude)
-        };
-        const k = Object.keys(station).join(', ');
-        const v = Object.keys(station).map((v, k) => `$${k + 1}`).join(', ');
-        pool.query(`INSERT INTO stations(${k}) VALUES(${v})`, Object.values(station), (err, res) => {
-          if (err) console.error(err);
-          if (res) rows--;
-          if (rows === 0) process.exit();
+    fs.createReadStream(`${__dirname}/stations.csv`).pipe(csv({ objectMode: true, columns: true }))
+        .on('data', (row) => {
+            rows++;
+            (async() => {
+                const station = {
+                    name: row.name,
+                    latitude: parseFloat(row.latitude),
+                    longitude: parseFloat(row.longitude),
+                    type: row.type
+                };
+                const k = Object.keys(station).join(', ');
+                const v = Object.keys(station).map((v, k) => `$${k + 1}`).join(', ');
+                pool.query(`INSERT INTO web.stations(${k}) VALUES(${v})`, Object.values(station), (err, res) => {
+                    if (err) console.error(err);
+                    if (res) rows--;
+                    if (rows === 0) process.exit();
+                });
+            })();
         });
-      })();
-    });
-  
 };
