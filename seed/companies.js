5716ac6e52bb6f521f1bca4e363bb9865865dfd9
Robert Gonzalez <robert@lightjab.com>
2017-09-29 15:34:46 -0500

    Updating companies

--- seed/companies.js
+++ seed/companies.js
@@ -3,21 +3,20 @@ const csv = require('csv-streamify');
 const fs = require('fs');
 
 const { Pool, Client } = require('pg');
-
 const pool = new Pool(config.get('pg'));
 
 exports.seed = () => {
-  let rows = 0;
-  fs.createReadStream(`${__dirname}/companies.csv`).pipe(csv({objectMode: true, columns: true}))
-    .on('data', (row) => {
-      const name = row.name;
-      rows++;
-      pool.query('INSERT INTO companies(name) VALUES($1)', [name], (err, res) => {
-        if (err) console.error(err);
-        if (res) {
-          rows--;
-        }
-        if (rows === 0) process.exit();
-      });
-    });
+    let rows = 0;
+    fs.createReadStream(`${__dirname}/companies.csv`).pipe(csv({ objectMode: true, columns: true }))
+        .on('data', (row) => {
+            const name = row.name;
+            rows++;
+            pool.query('INSERT INTO web.companies(name) VALUES($1)', [name], (err, res) => {
+                if (err) console.error(err);
+                if (res) {
+                    rows--;
+                }
+                if (rows === 0) process.exit();
+            });
+        });
 };
