c066d93eb50df29a5e42670b7d06c283be743f03
Anthony Shull <shull.anthony@gmail.com>
2017-10-02 11:28:40 -0500

    Get portfolio terminal information piecemeal

--- plugins/portfolio/queries/get_terminals_forecast_inventory.sql
+++ plugins/portfolio/queries/get_terminals_forecast_inventory.sql
@@ -1,8 +1,6 @@
-SELECT day,
-Cast(Sum(closing_stock) AS Integer) AS closing_stock
+SELECT *
 FROM web.inventory_forecast
 WHERE grade_id = $1
 AND terminal_id = ANY ($2)
 AND day BETWEEN $3 AND $4
-GROUP BY day
-ORDER BY day ASC;
+ORDER BY day ASC, terminal_id ASC, grade_id ASC;
--- plugins/portfolio/queries/get_terminals_inventory.sql
+++ plugins/portfolio/queries/get_terminals_inventory.sql
@@ -1,10 +1,6 @@
-SELECT day,
-Cast(Sum(volume_in) AS Integer) AS volume_in,
-Cast(Sum(volume_out) AS Integer) AS volume_out,
-Cast(Sum(closing_stock) AS Integer) AS closing_stock
+SELECT *
 FROM web.inventory_actual
 WHERE grade_id = $1
 AND terminal_id = ANY ($2)
 AND day BETWEEN $3 AND $4
-GROUP BY day
-ORDER BY day ASC;
+ORDER BY day ASC, terminal_id ASC, grade_id ASC;
