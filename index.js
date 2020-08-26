1a6cdba2abe396ddc66090c1cb73f2c4d46ee06f
Max Wade <max.wade@thirteen23.com>
2020-08-26 13:33:12 -0500

    Updated CORS

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const Hapi = require('hapi');
-const config = require('config');
-const redis = require('redis');
-const twilio = require('twilio');
-const parse = require('date-fns/parse');
-const subHours = require('date-fns/sub_hours');
-const addHours = require('date-fns/add_hours');
-const pg = require('pg');
+const path = require("path");
+const Hapi = require("hapi");
+const config = require("config");
+const redis = require("redis");
+const twilio = require("twilio");
+const parse = require("date-fns/parse");
+const subHours = require("date-fns/sub_hours");
+const addHours = require("date-fns/add_hours");
+const pg = require("pg");
 
 // pg.types.setTypeParser(1114, date => addHours(parse(date), 6));
 // pg.types.setTypeParser(1082, date => addHours(parse(date), 6));
@@ -17,44 +17,50 @@ module.exports = function(callback) {
   const server = new Hapi.Server();
 
   server.connection({
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: process.env.PORT || 9001,
     routes: {
-      cors: true,
+      cors: {
+        origin: ["*"],
+      },
     },
   });
 
-  const pool = new pg.Pool(config.get('pg'));
+  const pool = new pg.Pool(config.get("pg"));
 
-  server.decorate('server', 'pg', pool);
+  server.decorate("server", "pg", pool);
 
-  server.decorate('server', 'redis', redis.createClient(config.get('redis')));
+  server.decorate("server", "redis", redis.createClient(config.get("redis")));
 
-  server.decorate('server', 'twilio', new twilio(config.get('twilio').sid, config.get('twilio').secret));
+  server.decorate(
+    "server",
+    "twilio",
+    new twilio(config.get("twilio").sid, config.get("twilio").secret)
+  );
 
   server.register([
-    require('hapi-auth-jwt2'),
-    require('h2o2'),
+    require("hapi-auth-jwt2"),
+    require("h2o2"),
     {
-      register: require('./plugins/auth'),
+      register: require("./plugins/auth"),
     },
     {
-      register: require('./plugins/bus'),
+      register: require("./plugins/bus"),
     },
     {
-      register: require('./plugins/locations'),
+      register: require("./plugins/locations"),
     },
     {
-      register: require('./plugins/transports'),
+      register: require("./plugins/transports"),
     },
     {
-      register: require('./plugins/search'),
+      register: require("./plugins/search"),
     },
     {
-      register: require('./plugins/filter'),
+      register: require("./plugins/filter"),
     },
     {
-      register: require('./plugins/portfolio'),
+      register: require("./plugins/portfolio"),
     },
   ]);
 
