f3f3e2d754244fe8bb302d9f73a2987dd31000ef
Max Wade <max.wade@thirteen23.com>
2020-09-02 11:15:08 -0500

    Updated auth handler

--- plugins/auth/handlers.js
+++ plugins/auth/handlers.js
@@ -10,50 +10,20 @@ const { setCode, sendCode, verifyCode } = require("./auth");
 
 const users = [
   {
-    username: "anthonyshull",
-    phone: "12159090921",
-  },
-  {
-    username: "maxwade",
-    phone: "18179294543",
-  },
-  {
-    username: "tomhudson",
-    phone: "15126572608",
-  },
-  {
-    username: "dougcook",
-    phone: "15129647984",
-  },
-  {
-    username: "lanideguire",
-    phone: "15126892628",
-  },
-  {
-    username: "davidgreenberg",
-    phone: "13106141287",
-  },
-  {
-    username: "davidcorthorn",
-    phone: "61450208022",
-  },
-  {
-    username: "morgangerber",
-    phone: "16306218096",
-  },
-  {
-    username: "lauralevisay",
-    phone: "12545923703",
-  },
-  {
-    username: "alexchandy",
-    phone: "12813522263",
+    username: "joe.lene",
+    password: "magicshow",
   },
 ];
 
 // findName :: String -> Maybe String
-const findName = name => {
-  return find(user => equals(user.username, name), users);
+const findName = (name, password) => {
+  return users.find(
+    user => user.username === name && user.password === password
+  );
+  // return find(
+  //   user => equals(user.username, name) && equals(user.password, password),
+  //   users
+  // );
 };
 
 // lookUp :: String -> Maybe String
@@ -61,9 +31,15 @@ const lookUp = pipe([findName, map(prop("phone"))]);
 
 exports.authenticate = (req, rep) => {
   const uuid = uuidv1();
-  const { identifier } = req.payload;
+  const { identifier, password } = req.payload;
   const code = 0; //Math.ceil(Math.random() * (Math.floor(9999) - (Math.ceil(0))));
-  rep(uuid);
+  if (findName(identifier, password)) {
+    rep(uuid);
+  } else {
+    rep(Boom.unauthorized("Username and password combo not found."));
+  }
+
+  // rep(uuid);
   // parallel(1, [setCode(req.server.redis, uuid, code)]).fork(
   //   err => rep(err),
   //   res => rep(uuid)
